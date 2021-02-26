ac6cf4b1cc4b8b30a546d88c2d7eb871f1e750f6
Ramil Ashrafulin <HEHAHA@bk.ru>
2021-02-26 22:30:20 +0600

    fix: CORS

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://domainname.students.nomoreparties.space');
+  res.header('Access-Control-Allow-Origin', 'https://domainname.students.nomoreparties.space');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
